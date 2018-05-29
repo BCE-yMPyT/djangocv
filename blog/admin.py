58b1729522a2eb9fdff2b08a856b7f3c5797f3b0
BCE-yMPyT <askorytin94@gmail.com>
2018-05-29 11:54:42 +0300

    Made navigation system, comments and other stuff.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
+# C:\Program Files\PostgreSQL\9.3
 
 admin.site.register(Post)
+admin.site.register(Comment)
